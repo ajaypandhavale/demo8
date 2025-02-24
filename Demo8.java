b24b6c3d56ad10410ffce8a056fdfbbac27c03a4
ajaypandhavale <aj40135821@gmail.com>
2025-02-24 15:06:04 +0530

    updating to the repo

--- Demo8.java
+++ Demo8.java
@@ -2,6 +2,6 @@ public class Demo8 {
     public static void main(String[] args)
     {
         System.out.println("adding code to the repo");
-
+        System.out.println("updating code to the repo");
     }
 }
